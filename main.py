9d05297009616f1f74098f43535cab549c8e2531
suhnnylee21-arch <suhnnylee21@gmail.com>
2026-07-13 22:36:26 -0400

    Change DNA sequence input to user input

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-dna_sequence = "ATGCTAGCTAGCTAGCTAGGCTAA"
+dna_sequence = input('Enter DNA sequence: ').upper()
 Guanine = dna_sequence.count('G')
 print('The number of Guanine in the DNA sequence is', Guanine)
 Cytosine = dna_sequence.count('C')
@@ -7,7 +7,6 @@ total_length = len(dna_sequence)
 print('The total length of the DNA sequence is', total_length)
 gc_percentage = ((Guanine + Cytosine) / total_length) * 100
 print("The GC percentage of the DNA sequence is", gc_percentage)
-Part 2: 
 rna_sequence = dna_sequence.replace('T', 'U')
 print('The RNA sequence is', rna_sequence)
 start_codon_position = dna_sequence.find('ATG')
